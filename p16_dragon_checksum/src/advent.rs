848d7d92ee9f60cef8d27de2e3170f65e2a1b334
tinaun <tinagma@gmail.com>
2016-12-19 02:21:18 -0500

    problem 16 skeleton

---
+++ p16_dragon_checksum/src/advent.rs
@@ -0,0 +1,20 @@
+/// advent of code 2016 
+///
+/// problem 16
+/// generate pseudorandom data + compute checksum
+
+pub static PROBLEM_NUMBER: &'static str = "16"; 
+
+pub fn generate(inital: &str, length: u64) -> String {
+
+
+}
+
+pub fn checksum(input: &str, length: u64 ) -> String {
+    
+}
+
+pub fn adv_main(input: Vec<String>) {
+
+}
+
