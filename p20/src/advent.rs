a84c8344b7f58e5b6b2eae98cf3599df59b87770
tinaun <tinagma@gmail.com>
2016-12-20 00:52:58 -0500

    problem 20, both parts

---
+++ p20/src/advent.rs
@@ -0,0 +1,44 @@
+/// advent of code 2016 
+///
+/// problem 20
+/// ip range filtering
+
+
+/// part 1
+/// cargo run --release input.txt > output.txt
+/// head -1 output.txt
+
+/// part 2
+/// wc -l output.txt
+
+pub static PROBLEM_NUMBER: &'static str = "20"; 
+
+use std::collections::BTreeSet;
+
+pub fn adv_main(input: Vec<String>) {
+    let mut ranges: BTreeSet<(u32, u32)> = BTreeSet::new();
+
+    for line in input {
+        if line.len() > 1 {
+            let limits: Vec<_> = line.split("-").collect();
+
+            let (low, high) = (limits[0].parse::<u32>().unwrap(), limits[1].parse::<u32>().unwrap());
+
+            ranges.insert( (low, high) );
+        }
+    }
+
+    let mut lasthigh = 0;
+    for (l, h) in ranges {
+        if (lasthigh) < l-1 && l > 0 {
+            println!("gap! - {} ", lasthigh + 1);
+        }
+
+        if h > lasthigh {
+            lasthigh = h;
+        }
+    }
+
+}
+
+
